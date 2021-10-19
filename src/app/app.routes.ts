f9c889e284f969363a8023fa40de6fc7e9ccda20
nandobrazil <contato@frinfo.com.br>
2021-10-19 12:27:59 -0400

    Criação de components: about, restaurants, home e acrescentando suas rotas

---
+++ src/app/app.routes.ts
@@ -0,0 +1,18 @@
+import { RestaurantsComponent } from './restaurants/restaurants.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { RouterModule, Routes } from "@angular/router";
+import { NgModule } from '@angular/core';
+
+const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'about', component: AboutComponent},
+    {path: 'restaurants', component: RestaurantsComponent}
+
+]
+
+@NgModule({
+    imports:[RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRouterModule { }
